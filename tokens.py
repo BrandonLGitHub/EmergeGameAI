7177161e7bfe2107f82f578bf7da30dd24379ffe
BrandonLGitHub <h.lange.brandon@gmail.com>
2024-03-14 13:31:33 -0400

    Token functions module

---
+++ tokens.py
@@ -0,0 +1,8 @@
+#   TODO checks research tokens to see if they are usable for this round
+def use_token(tokens, feature: object = None) -> object:
+    return None
+
+
+#   TODO create function that purchases tokens
+def buy_tokens(dice):
+    return None
