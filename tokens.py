c27713ddecadb878bbc6025a01c70668cae76dea
BrandonLGitHub <h.lange.brandon@gmail.com>
2024-03-20 13:20:21 -0400

    Created token class

--- tokens.py
+++ tokens.py
@@ -1,3 +1,11 @@
+
+class research_token:
+    def __init__(self, feature, num, effect):
+        self.feature = feature
+        self.num = num
+        self.effect = effect
+
+
 #   TODO checks research tokens to see if they are usable for this round
 def use_token(tokens, feature: object = None) -> object:
     return None
