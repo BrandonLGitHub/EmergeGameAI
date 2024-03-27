8cf6316fe1f8b91662f350e19be42e927028bfd5
Brandon <h.lange.brandon@gmail.com>
2024-03-27 13:12:22 -0400

    Moved player_turn() to avoid circular import

--- SandyAi/sandy_ai.py
+++ SandyAi/sandy_ai.py
@@ -1,9 +1,9 @@
 from typing import Any
-import tokens
+import SandyAi.tokens as tokens
 from SandyAi import decide as dm, dice as dice_func, modifiers as mod, scoring as score
-import sys
 
 
+# TODO docstrings
 def game_runner():
     #   initial modifier set up
     modifiers = {'Plants': [1], 'Crab': [2], 'Turtle': [3], 'Seal': [4], 'Tectonic': [5], 'Bird': [6]},
@@ -31,28 +31,18 @@ def game_runner():
         modifiers = mod.set_modifiers(islands)
         print("My modifiers are all set! Once you have set yours let's continue.\nIf its your turn to go first, Take "
               "your turn now and then you can tell me to go :)")
-        player_turn()
+        dm.player_turn()
         # sets dice based off of round number
         dice['dice_amt'] = dice_func.dice_count(current_round)
         dice['roll_result'] = dice_func.roll_dice(dice['dice_amt'], dice['saved_dice'])
         islands, land_birds, dice['saved_dice'], tokens_held = dm.spend_dice(islands, modifiers, dice, land_birds, tokens_held)
-        tokens_held, dice['saved_dice'] = tokens.buy_tokens(tokens_held, dice['saved_dice'])
+        #   TODO add below code once token systems is working
+        #   tokens_held, dice['saved_dice'] = tokens.buy_tokens(tokens_held, dice['saved_dice'])
         print(f'End of round {current_round}\n')
 
     final_score = score.get_score(islands)
     print(f"Great game!!! My final score was {final_score}. I hope you had as much fun as I did. I'll be waiting for a "
           f"rematch!")
-
-
-def player_turn():
-    response = input("Enter 'Y' to continue or 'q' to quit the game.\n").strip().lower()
-    if response == 'y':
-        return
-    elif response == 'q':
-        sys.exit()
-    else:
-        print('Invalid response. Please enter "Y" to continue or "q" to quit the game.')
-        player_turn()
 
 
 if __name__ == '__main__':
